82cd742b625f4a369fa7c909d82330eb9a4950d5
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-10-15 11:12:33 -0700

    Sign in page works and stores username in cookie

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ app.use(cookieParser());
 app.get('/', (request, response) => {
 	response.render('index');
 });
+app.get('/sign_in', (request, response) => {
+	response.render('sign_in');
+});
+
+const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24; // keep for one day
+app.post("/sign_in", (request, response) => {
+  const username = request.body.username;
+  console.log(`username: ${username}`)
+  response.cookie(username, { maxAge: COOKIE_MAX_AGE });
+  response.redirect("/");
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
