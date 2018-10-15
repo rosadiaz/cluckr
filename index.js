6bebc7838a98d00e7db0421a6ecc966cec2ac03a
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-10-15 12:31:37 -0700

    Sign in actually works now, and so does sign-out

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-const path = require('path')
+const path = require('path');
 app.use(express.static(path.join(__dirname, "public")))
 
 const morgan = require('morgan');
@@ -14,19 +14,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-app.get('/', (request, response) => {
-	response.render('index');
+app.get('/', (req, res) => {
+  username = req.cookies.username;
+	res.render('index', {username});
 });
-app.get('/sign_in', (request, response) => {
-	response.render('sign_in');
+
+app.get('/sign_in', (req, res) => {
+	res.render('sign_in');
+});
+app.post('/sign_out', (req, res) => {
+  console.log('post from sign out')
+	res.clearCookie('username');
+  res.redirect('/');
 });
 
 const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24; // keep for one day
-app.post("/sign_in", (request, response) => {
-  const username = request.body.username;
-  console.log(`username: ${username}`)
-  response.cookie(username, { maxAge: COOKIE_MAX_AGE });
-  response.redirect("/");
+app.post('/sign_in', (req, res) => {
+  const username = req.body.username;
+  console.log(`🍪 username in cookie: ${username}`)
+  res.cookie('username', username, { maxAge: COOKIE_MAX_AGE });
+  res.redirect("/");
 });
 
 const PORT = 5000;
